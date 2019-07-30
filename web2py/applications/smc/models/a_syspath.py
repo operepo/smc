1878bd2f5c8a7f61916a919407df38913d4dd9ea
Ray Pulsipher <ray@cmagic.biz>
2019-07-30 12:54:42 -0700

    - add visible version to smc footer

--- web2py/applications/smc/models/a_syspath.py
+++ web2py/applications/smc/models/a_syspath.py
@@ -8,13 +8,16 @@ track_changes(True)
 import os
 import sys
 
-spath_imported = False
+global APP_VERSION
+APP_VERSION = None
+
+sys_path_imported = False
 
 # Needed because w2p import sucks and can't find modules in the modules folder
 import_path = os.path.join(request.folder, 'modules')
 if import_path not in sys.path:
     sys.path.append(import_path)
-    spath_imported = True
+    sys_path_imported = True
 
 
 # If windows, write a kill bat file to end the web2py process
@@ -26,11 +29,11 @@ if os.name is 'nt' and request.is_scheduler is False:
     bat_path = os.path.join(curr_path, "kill_smc.bat")
     if os.path.exists(bat_path) is not True:
         print("Writing " + str(bat_path))
-        f = open(bat_path, 'w')
-        f.write("@echo off\r\ntaskkill /f /pid ")
-        f.write(str(os.getpid()))
-        f.write("\r\n")
-        f.close()
+        kill_smc_f = open(bat_path, 'w')
+        kill_smc_f.write("@echo off\r\ntaskkill /f /pid ")
+        kill_smc_f.write(str(os.getpid()))
+        kill_smc_f.write("\r\n")
+        kill_smc_f.close()
 
 
 def get_app_folders():
@@ -54,20 +57,39 @@ def get_app_folders():
     # media folder
     # media_folder = os.path.join(static_folder, "media")
 
-    #controllers_folder = os.path.join(app_folder, "controllers")
-    #cron_folder = os.path.join(app_folder, "cron")
-    #databases_folder = os.path.join(app_folder, "databases")
-    #errors_folder = os.path.join(app_folder, "errors")
-    #languages_folder = os.path.join(app_folder, "languages")
-    #private_folder = os.path.join(app_folder, "private")
-    #modules_folder = os.path.join(app_folder, "modules")
-    #sessions_folder = os.path.join(app_folder, "sessions")
-    #uploads_folder = os.path.join(app_folder, "uploads")
-    #views_folder = os.path.join(app_folder, "views")
-
-    # win ffmpeg folder
-    #win_ffmpeg_folder = os.path.join(w2py_folder, "ffmpeg", "bin")
+    # controllers_folder = os.path.join(app_folder, "controllers")
+    # cron_folder = os.path.join(app_folder, "cron")
+    # databases_folder = os.path.join(app_folder, "databases")
+    # errors_folder = os.path.join(app_folder, "errors")
+    # languages_folder = os.path.join(app_folder, "languages")
+    # private_folder = os.path.join(app_folder, "private")
+    # modules_folder = os.path.join(app_folder, "modules")
+    # sessions_folder = os.path.join(app_folder, "sessions")
+    # uploads_folder = os.path.join(app_folder, "uploads")
+    # views_folder = os.path.join(app_folder, "views")
 
     return w2py_folder, applications_folder, app_folder
 
 
+def get_app_version():
+    global APP_VERSION
+
+    if APP_VERSION is None or APP_VERSION == "":
+        # Dummy value - use this in case reading the file fails
+        APP_VERSION = "v1.9"
+
+        (w2py_folder, applications_folder, app_folder) = get_app_folders()
+
+        app_ver_path = os.path.join(app_folder, 'private', 'app_version.txt')
+        if os.path.exists(app_ver_path):
+            try:
+                app_ver_path_f = open(app_ver_path, 'r')
+                APP_VERSION = app_ver_path_f.read().strip()
+                app_ver_path_f.close()
+            except Exception as ex:
+                print("Error reading the app version file, using default: " + str(APP_VERSION) + "<br />" + str(ex))
+        else:
+            # print("No private/app_version.txt exists... Setting to " + str(APP_VERSION))
+            pass
+
+    return APP_VERSION
