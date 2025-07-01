2d072b52c9ed08f7d8c011f936243b17f7279c9f
MustafaAlshehab <mustafa.alshehab@hotmail.com>
2025-07-01 22:15:49 +0000

    added main path printout

--- clear_pyc_files.py
+++ clear_pyc_files.py
@@ -1,16 +1,11 @@
 import os
 
-#top = "c:\\python37-32"
-
 top = os.path.dirname(os.path.abspath(__file__))
+print(f"Removing .pyc and .pyo files in {top}")
 
 for root, dirs, files in os.walk(top, topdown=False):
     for name in files:
         if name.endswith(".pyc") or name.endswith(".pyo"):
             print("Removing " + name)
             os.remove(os.path.join(root, name))
-    #for name in dirs:
-    #    os.rmdir(os.path.join(root, name))
-    
 
-#r = input("Done!")
